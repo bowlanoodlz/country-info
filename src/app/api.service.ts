42da9fc449899c560455515db661aca9c12989a6
Justin <jmalot2@wgu.edu>
2024-05-13 13:31:13 -0500

    Added api calls for country info

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { XmlParser } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,25 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   fetchCountryInfo(country: string){
-    let api = `http://api.worldbank.org/V2/country/${country}?/indicator/SP.POP.TOTL?format=json`
+    //let api = `http://api.geonames.org/countryInfoJSON?lang=EN&country=${country}&username=malo92`
+      let api = `http://api.worldbank.org/v2/country/${country}?format=JSON`
 
     return this.http.get(api)
+  }
+
+  setCountryInfo(country: string){
+    let subject = new Subject();
+
+    this.fetchCountryInfo(country).subscribe((data: any) => {
+      subject.next({
+        countrySelected: data[1][0].name,
+        countryCapital: data[1][0].capitalCity,
+        region: data[1][0].region.value,
+        income: data[1][0].incomeLevel.value,
+        countryLat: data[1][0].latitude,
+        countryLong: data[1][0].longitude        
+      })
+    })
+    return subject.asObservable();
   }
 }
--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-world-map',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './world-map.component.html',
   styleUrl: './world-map.component.css'
 })
 export class WorldMapComponent {
+  countryData: any = {};
 
   constructor (private apiService: ApiService) {}
 
 setCountryData(event: any) {
-  console.log("event", event.target.id);
-  this.apiService.fetchCountryInfo(event.target.id).subscribe(data => console.log(data))
+  //console.log("event", event.target.id);
+  this.apiService.setCountryInfo(event.target.id).subscribe((data: any) => {
+    //console.log(data)
+    this.countryData = {
+      ...data,
+      state:event.target.getAttribute("title")
+    }
+  })
 }
 
 }
